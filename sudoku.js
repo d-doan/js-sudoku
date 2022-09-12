e57a5d3ca3d394afba1728dce31646ca8233e69c
d-doan <44249543+d-doan@users.noreply.github.com>
2022-09-12 15:56:20 -0700

    added js for bottom bar

---
+++ sudoku.js
@@ -0,0 +1,32 @@
+
+var errors = 0;
+
+var numSelected;
+var squareSelected;
+
+
+var board = [
+
+]
+
+var solutionBoard = [
+
+]
+
+window.onload = function () {
+    createGame();
+}
+
+function createGame() {
+
+    for (let i = 1; i < 10; i++) {
+        let number = document.createElement("div");
+        number.id = i
+        number.innerText = i;
+        number.classList.add("number");
+        number.classList.add("number");
+        document.getElementById("numberBar").appendChild(number);
+    }
+}
+
+
