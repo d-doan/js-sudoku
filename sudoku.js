7676a5eb7e4f9affc485de25f8b45460b07f6ad1
Matthew Sun <matthewrsun06@gmail.com>
2022-09-12 17:15:22 -0700

    Auto-create board

--- sudoku.js
+++ sudoku.js
@@ -20,8 +20,21 @@ window.onload = function () {
 function createGame() {
 
     for (let i = 1; i < 10; i++) {
+        let grid = document.createElement("div");
+        grid.id = i;
+        grid.classList.add("grid");
+        document.getElementById("board").appendChild(grid);
+        for (let j = 1; j < 10; j++) {
+            let num = document.createElement("div");
+            num.id = j;
+            num.classList.add("box");
+            grid.appendChild(num);
+        }
+    }
+    
+    for (let i = 1; i < 10; i++) {
         let number = document.createElement("div");
-        number.id = i
+        number.id = i;
         number.innerText = i;
         number.classList.add("number");
         document.getElementById("numberBar").appendChild(number);
